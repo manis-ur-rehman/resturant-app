9f01c9efa348405159f1ee68267990b585f249f7
manis-ur-rehman <143801677+manis-ur-rehman@users.noreply.github.com>
2023-09-25 00:29:54 +0500

    dashboard layout create

---
+++ app/components/SideNavBar.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const SideNavBar = () => {
+  return <div className="flex-none w-20 bg-gray-200 h-screen ">01</div>;
+};
+
+export default SideNavBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import SignUp from "@login/page";
-import Login from "@signup/page";
+import SignUp from "@signup/page";
+import Login from "@login/page";
+import SideNavBar from "@components/SideNavBar";
+import Header from "@components/Header";
 
 export default function Home() {
   return (
     <main>
       {/* <Login /> */}
       <SignUp />
+      {/* <div className="flex">
+        <SideNavBar />
+        <div className="flex-1 md:flex h-screen relative">
+          <Header />
+        </div>
+      </div> */}
     </main>
   );
 }
