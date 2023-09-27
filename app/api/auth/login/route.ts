23d701372c63a7646d17fe5db697958e44751fb9
manis-ur-rehman <143801677+manis-ur-rehman@users.noreply.github.com>
2023-09-27 14:00:21 +0500

    prisma db add for user

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,10 +1,26 @@
 import { StorageType } from "@/globaltypes";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const bcript = require("bcrypt");
 
 export async function POST(req: Request) {
   const body = await req.json();
   const cookieStore = cookies();
+  const { email } = body;
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  if (user) {
+    console.log("exist: ", user);
+  } else {
+    console.log("not found");
+  }
   cookieStore.set(StorageType.TOKEN, "1234");
   return NextResponse.json(body, { status: 200 });
 }
--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -1,18 +1,8 @@
-import type { NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
-type Data = {
-  name: string;
-};
-
-export async function GET(res: NextApiResponse<Data>) {
-  console.log("prisma: ", prisma);
-  //   await prisma.abcd.deleteMany();
-
-  await prisma.abcd.createMany({
-    data: [{ name: "anees" }, { name: "basit" }, { name: "shahzad" }],
-  });
-
-  res.status(200).json({ name: "hello" });
+export async function GET() {
+  return NextResponse.json(
+    { restaurantApplication: "running" },
+    { status: 200 }
+  );
 }
